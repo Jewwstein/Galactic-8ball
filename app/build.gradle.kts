f68d7e8e5bb8585e40373c0e0d44fd680573589f
Jewwstein <jsgtab@gmail.com>
2026-09-23 14:02:39 -0500

    Initialize Galactic 8-Ball Android project

---
+++ app/build.gradle.kts
@@ -0,0 +1,6 @@
+plugins { id("com.android.application") }
+android {
+ namespace = "com.galactic.eightball"
+ compileSdk = 35
+ defaultConfig { applicationId = "com.galactic.eightball"; minSdk = 26; targetSdk = 35; versionCode = 1; versionName = "0.1" }
+}
