b1599e49f82564247cef266f68f3e761a5f2cf0b
Jewwstein <jsgtab@gmail.com>
2026-09-25 15:00:44 -0500

    Bump Android test version for swipe aiming

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
    applicationId = "com.galactic.eightball"
    minSdk = 26
    targetSdk = 35
-   versionCode = 7
-   versionName = "0.7"
+   versionCode = 8
+   versionName = "0.7.1"
  }
 
  buildTypes {
