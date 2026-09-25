57fb38f1924cd4bc8a4f86fd8d0010bd9dd08eeb
Jewwstein <jsgtab@gmail.com>
2026-09-25 18:16:53 -0500

    Bump version for progression update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
    applicationId = "com.galactic.eightball"
    minSdk = 26
    targetSdk = 35
-   versionCode = 12
-   versionName = "0.7.5"
+   versionCode = 13
+   versionName = "0.7.6"
  }
 
  buildTypes {
