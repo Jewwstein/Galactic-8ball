21233b9e794be26e970baf89b9a51e400434294d
Jewwstein <jsgtab@gmail.com>
2026-09-26 03:05:26 -0500

    Bump version to 0.8.5 build 22

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
    applicationId = "com.galactic.eightball"
    minSdk = 26
    targetSdk = 35
-   versionCode = 21
-   versionName = "0.8.4"
+   versionCode = 22
+   versionName = "0.8.5"
  }
 
  buildTypes {
