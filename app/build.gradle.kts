215ed96dd9605ffa75e760420c4f0d8ee88f7d89
Jewwstein <jsgtab@gmail.com>
2026-09-26 03:49:26 -0500

    Bump to v0.8.7 build 24

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
    applicationId = "com.galactic.eightball"
    minSdk = 26
    targetSdk = 35
-   versionCode = 23
-   versionName = "0.8.6"
+   versionCode = 24
+   versionName = "0.8.7"
  }
 
  buildTypes {
