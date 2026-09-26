b4b84ae086735647fe48a48bf0eb2a0e6743d42c
Jewwstein <jsgtab@gmail.com>
2026-09-25 19:41:40 -0500

    Bump version for approved reward hilt art

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
    applicationId = "com.galactic.eightball"
    minSdk = 26
    targetSdk = 35
-   versionCode = 13
-   versionName = "0.7.6"
+   versionCode = 14
+   versionName = "0.7.7"
  }
 
  buildTypes {
