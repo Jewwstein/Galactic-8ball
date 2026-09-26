6232c6ade4f34e89bb2f78cd7f7f46804792441e
Jewwstein <jsgtab@gmail.com>
2026-09-26 03:17:55 -0500

    Bump to v0.8.6 build 23 Maul reward update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
    applicationId = "com.galactic.eightball"
    minSdk = 26
    targetSdk = 35
-   versionCode = 22
-   versionName = "0.8.5"
+   versionCode = 23
+   versionName = "0.8.6"
  }
 
  buildTypes {
