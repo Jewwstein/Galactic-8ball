ef439b6904cf31433ff1a20da9831ae5b95ef0f2
Jewwstein <jsgtab@gmail.com>
2026-09-26 17:46:02 -0500

    Bump to v0.8.18 build 35 Blue Wraith challenge

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
    applicationId = "com.galactic.eightball"
    minSdk = 26
    targetSdk = 35
-   versionCode = 34
-   versionName = "0.8.17"
+   versionCode = 35
+   versionName = "0.8.18"
  }
 
  buildTypes {
