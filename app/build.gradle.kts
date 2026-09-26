671378c422115373f9261c6d7ea8f1804ec781df
Jewwstein <jsgtab@gmail.com>
2026-09-26 16:58:25 -0500

    Bump to v0.8.17 build 34 Mystic Ninja Blade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
    applicationId = "com.galactic.eightball"
    minSdk = 26
    targetSdk = 35
-   versionCode = 33
-   versionName = "0.8.16"
+   versionCode = 34
+   versionName = "0.8.17"
  }
 
  buildTypes {
