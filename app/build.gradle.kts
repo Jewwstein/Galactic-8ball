075df74c797015f220cfc5a9c7054b5ff6ba95e7
Jewwstein <jsgtab@gmail.com>
2026-09-26 16:45:30 -0500

    Bump to v0.8.16 build 33 Babsy Blade challenge

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
    applicationId = "com.galactic.eightball"
    minSdk = 26
    targetSdk = 35
-   versionCode = 32
-   versionName = "0.8.15"
+   versionCode = 33
+   versionName = "0.8.16"
  }
 
  buildTypes {
