2db503e6c707390d6d742173051c690b713a75a1
Jewwstein <jsgtab@gmail.com>
2026-09-26 14:29:20 -0500

    Bump to v0.8.11 build 28 HD hilt artwork

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
    applicationId = "com.galactic.eightball"
    minSdk = 26
    targetSdk = 35
-   versionCode = 27
-   versionName = "0.8.7"
+   versionCode = 28
+   versionName = "0.8.11"
  }
 
  buildTypes {
