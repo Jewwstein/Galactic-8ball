94d221633bd14068ad2d981dcc15b6a6fb8439d9
Jewwstein <jsgtab@gmail.com>
2026-09-25 21:07:09 -0500

    Load repaired reward hilt WebP assets

--- app/src/main/java/com/galactic/eightball/MainActivity.java
+++ app/src/main/java/com/galactic/eightball/MainActivity.java
@@ -1591,7 +1591,7 @@ public class MainActivity extends Activity {
     final Paint glow=new Paint(Paint.ANTI_ALIAS_FLAG);
     final Bitmap[] hilts=new Bitmap[6],rewardHilts=new Bitmap[7],blades=new Bitmap[6];
     final String[] hiltFiles={"hilt_thumb_0.png","hilt_thumb_1.png","hilt_thumb_2.png","hilt_thumb_3.png","hilt_thumb_4.png","hilt_thumb_5.png"};
-    final String[] rewardHiltFiles={"yoda.png","ahsoka.png","anakin.png","nihilus.png","stormtrooper.png","kylo.png","rey.png"};
+    final String[] rewardHiltFiles={"yoda.png","ahsoka.webp","anakin.png","nihilus.webp","stormtrooper.png","kylo.png","rey.webp"};
     final String[] bladeFiles={"blade_dark.png","blade_gold.png","blade_purple.png","blade_green.png","blade_red.png","blade_blue.png"};
     final int[] bladeColors={0xFFEAF7FF,0xFFFFC54A,0xFFB064FF,0xFF48FF7A,0xFFFF3D38,0xFF4DA8FF};
     volatile long pulseUntil=0;
@@ -1853,7 +1853,7 @@ public class MainActivity extends Activity {
     };
 
     final String[] hiltFiles={"hilt_thumb_0.png","hilt_thumb_1.png","hilt_thumb_2.png","hilt_thumb_3.png","hilt_thumb_4.png","hilt_thumb_5.png"};
-    final String[] rewardHiltFiles={"yoda.png","ahsoka.png","anakin.png","nihilus.png","stormtrooper.png","kylo.png","rey.png"};
+    final String[] rewardHiltFiles={"yoda.png","ahsoka.webp","anakin.png","nihilus.webp","stormtrooper.png","kylo.png","rey.webp"};
     final String[] bladeFiles={"blade_dark.png","blade_gold.png","blade_purple.png","blade_green.png","blade_red.png","blade_blue.png"};
     final String[] hiltNames={"OBI-WAN","LUKE BLUE","MACE WINDU","DARTH MAUL","LUKE GREEN","DARTH VADER",
       "YODA","AHSOKA FULCRUM","ANAKIN CLASSIC","DARTH NIHILUS","STORMTROOPER","KYLO REN","REY"};
