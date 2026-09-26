d2798d59d0dba8c00e1a7c2031a97f4d995d6144
Jewwstein <jsgtab@gmail.com>
2026-09-26 17:59:11 -0500

    Bump to v0.8.19 build 36 moving Thumb Strike

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
    applicationId = "com.galactic.eightball"
    minSdk = 26
    targetSdk = 35
-   versionCode = 35
-   versionName = "0.8.18"
+   versionCode = 36
+   versionName = "0.8.19"
  }
 
  buildTypes {
