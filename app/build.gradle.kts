ae6cb3e3c6b254dc1b844bf2aaaa298846c5b0e9
Jewwstein <jsgtab@gmail.com>
2026-09-26 00:57:07 -0500

    Bump Galactic 8-Ball to 0.8.3 build 20

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
    applicationId = "com.galactic.eightball"
    minSdk = 26
    targetSdk = 35
-   versionCode = 19
-   versionName = "0.8.2"
+   versionCode = 20
+   versionName = "0.8.3"
  }
 
  buildTypes {
